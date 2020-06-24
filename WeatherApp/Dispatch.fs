c6139d0530c9c7434e4cb0da0260f522fce9ad36
Zach Howe <zach@zachhowe.com>
2020-06-24 00:35:55 -0700

    Add Dispatch.backgroundAsync

--- WeatherApp/Dispatch.fs
+++ WeatherApp/Dispatch.fs
@@ -5,3 +5,6 @@ open CoreFoundation
 module Dispatch =
     let mainAsync (x: unit -> unit) = 
         DispatchQueue.MainQueue.DispatchAsync x
+
+    let backgroundAsync (x: unit -> unit) =
+        DispatchQueue.GetGlobalQueue(DispatchQualityOfService.Background).DispatchAsync x
