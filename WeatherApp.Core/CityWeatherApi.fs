6d91e34668ff633d21c857b20eecf28c3e7004b4
Zach Howe <zach@zachhowe.com>
2020-06-23 22:35:09 -0700

    Refactors

---
+++ WeatherApp.Core/CityWeatherApi.fs
@@ -0,0 +1,31 @@
+﻿namespace WeatherApp.Core
+
+open FSharp.Control
+open FSharp.Control.Reactive
+open FSharp.Control.Reactive.Builders
+
+module public CityWeatherApi =
+    type City with
+        static member private FromWireType (city: CityWeatherType.Root) =
+            { City = { ID = city.Id; Name = city.Name }
+              Weather = { Temp = city.Main.Temp; FeelsLike = city.Main.FeelsLike } }
+
+    type CityWeatherRequest =
+    | Query of string
+    | CityID of int
+    | Location of float * float
+        member private this.UrlStringForRequest appId =
+            let uriString = "https://api.openweathermap.org/data/2.5/weather"
+            let apiKey = ("appid", appId)
+            match this with
+            | Query(query) -> uriString + "?" + UriQueryBuilder.formatQueryString [ apiKey; ("q", query) ]
+            | CityID(cityId) -> uriString + "?" + UriQueryBuilder.formatQueryString [ apiKey; ("id", (string cityId)) ]
+            | Location(lat, lon) -> uriString + "?" + UriQueryBuilder.formatQueryString [ apiKey; ("lat", (string lat)); ("lon", (string lon))]
+        
+        member public this.LoadWeather appId = observe {
+            let url = this.UrlStringForRequest appId
+            let request = CityWeatherType.AsyncLoad url
+                            |> Observable.ofAsync
+                            |> Observable.map City.FromWireType
+            yield! request
+        }
--- WeatherApp.Core/CityWeatherProvider.fs
+++ WeatherApp.Core/CityWeatherProvider.fs
@@ -4,19 +4,19 @@ open FSharp.Control
 open FSharp.Control.Reactive
     
 module public CityWeatherProvider =
-    open CityWeatherRequest
+    open CityWeatherApi
    
     let private appId = "2095b1e3413a7e02ad01ee92b227a889"
 
-    let private toLoadCityRequest request =
-        loadWeather appId request
+    let private toLoadCityRequest (request: CityWeatherRequest) =
+        request.LoadWeather appId
         
     let private requestOfQuery query =
         match query with
-        | WeatherQuery.Query(query) -> Request.Query query |> List.ofOne
-        | WeatherQuery.CityID(cityId) -> Request.CityID cityId |> List.ofOne
-        | WeatherQuery.CityIDs(cityIds) -> cityIds |> List.map Request.CityID
-        | WeatherQuery.Location(lat, lon) -> Request.Location (lat, lon) |> List.ofOne
+        | WeatherQuery.Query(query) -> Query query |> List.ofOne
+        | WeatherQuery.CityID(cityId) -> CityID cityId |> List.ofOne
+        | WeatherQuery.CityIDs(cityIds) -> cityIds |> List.map CityID
+        | WeatherQuery.Location(lat, lon) -> Location (lat, lon) |> List.ofOne
 
     let getCityWeather (query: WeatherQuery) = 
         query
